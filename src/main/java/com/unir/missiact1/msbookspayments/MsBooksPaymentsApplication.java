27cbf4b65a71841456762f2697e683ef6be787df
Renzo Di Paola Jara <rdipaolaj@outlook.com>
2025-06-27 22:59:13 -0500

    first commit

---
+++ src/main/java/com/unir/missiact1/msbookspayments/MsBooksPaymentsApplication.java
@@ -0,0 +1,13 @@
+package com.unir.missiact1.msbookspayments;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsBooksPaymentsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsBooksPaymentsApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/unir/missiact1/msbookspayments/controller/HealthController.java
@@ -0,0 +1,14 @@
+package com.unir.missiact1.msbookspayments.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+    
+    @GetMapping("/health")
+    public ResponseEntity<String> health() {
+        return ResponseEntity.ok("Microservice Ok");
+    }
+}
